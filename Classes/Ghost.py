c0d5c0c069edce748e41a76e63fc32905e1b2f63
Phylisophyisawasteoftime <oshriyoungname1234@gmail.com>
2023-03-29 20:52:16 +0300

    full pacman animation plus error trying to make the ghosts chase pacman.

--- Classes/Ghost.py
+++ Classes/Ghost.py
@@ -6,40 +6,29 @@ from Scripts.constants import *
 class Ghost(pygame.sprite.Sprite):
     def __init__(self, x, y, color):
         super().__init__()
-        self.direction = None
         self.image = pygame.Surface((20, 20))
         self.image.fill(color)  # Set the color of the ghost
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.speed = GHOST_SPEED
+        self.direction = "left"
 
-    def change_direction(self, pacman):
-        directions = ["L", "R", "U", "D"]
+    def change_direction(self):
+        directions = ["left", "right", "up", "down"]
         if self.rect.x % 16 == 0 and self.rect.y % 16 == 0:
-            x1 = self.rect.x
-            y1 = self.rect.y
-            x2 = pacman.x
-            y2 = pacman.y
-            if x1 < x2:
-                self.direction = random.choice(directions)
-            elif x1 > x2:
-                self.direction = random.choice(directions)
-            elif y1 < y2:
-                self.direction = random.choice(directions)
-            elif y1 > y2:
-                self.direction = random.choice(directions)
+            self.direction = random.choice(directions)
 
     def collide_with_walls(self, walls):
         for wall in walls:
             if self.rect.colliderect(wall.rect):
-                if self.direction == "R":
+                if self.direction == "right":
                     self.rect.right = wall.rect.left
-                elif self.direction == "L":
+                elif self.direction == "left":
                     self.rect.left = wall.rect.right
-                elif self.direction == "U":
+                elif self.direction == "up":
                     self.rect.top = wall.rect.bottom
-                elif self.direction == "D":
+                elif self.direction == "down":
                     self.rect.bottom = wall.rect.top
 
     def update(self, walls, pacman):
@@ -53,4 +42,4 @@ class Ghost(pygame.sprite.Sprite):
             self.rect.y += self.speed
 
         self.collide_with_walls(walls)
-        self.change_direction(pacman)
+        self.change_direction()
